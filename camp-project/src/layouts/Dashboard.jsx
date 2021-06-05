7fae8a3c15b47fcbdbeb0e046f4ef2999c92ee12
Batuhan Sahin <battusahin97@gmail.com>
2021-06-06 01:18:43 +0300

    React baslangic

---
+++ camp-project/src/layouts/Dashboard.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import ProductList from '../pages/ProductList'
+import Categories from './Categories'
+import { Grid } from 'semantic-ui-react'
+
+
+
+export default function Dashboard() {
+    return (
+        <div>
+            <Grid columns={3}>
+                <Grid.Row>
+                    <Grid.Column width={4}>
+                        <Categories></Categories>
+                    </Grid.Column>
+                    <Grid.Column width={12}>
+                    <ProductList></ProductList> 
+                    </Grid.Column>
+                </Grid.Row>
+            </Grid>
+            
+        </div>
+    )
+}
